233c5b608e4f233f9385d97ca9669824b05dbb48
Nhu Vu <nhu.vu48@gmail.com>
2024-07-19 08:27:08 +0700

    add new exercises L11-Blue

--- BlueCourse/src/L11_BinarySearch/Pizzamania.java
+++ BlueCourse/src/L11_BinarySearch/Pizzamania.java
@@ -3,5 +3,46 @@
  * */
 package L11_BinarySearch;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Pizzamania {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int testcase = scanner.nextInt();
+        for(int tc = 0; tc < testcase; tc++){
+            int n = scanner.nextInt();
+            int k = scanner.nextInt();
+            int[] a = new int[n];
+            for(int i = 0 ; i < n; i++){
+                a[i] = scanner.nextInt();
+            }
+            Arrays.sort(a);
+            int count = 0;
+            for(int i = 0; i < n; i++){
+                int current = a[i];
+                int lookUp = k - current;
+                int result = binarySearch(a, i + 1, n - 1, lookUp);
+                if(result != -1){
+                    count++;
+                }
+            }
+            System.out.println(count);
+
+        }
+    }
+
+    public static int binarySearch(int[] a, int left, int right, int x){
+        if(left <= right){
+            int mid = left + (right - left)/2;
+            if(a[mid] == x){
+                return mid;
+            }else if(x > a[mid]){
+                return binarySearch(a, mid + 1, right, x);
+            }else {
+                return binarySearch(a, left, mid - 1, x);
+            }
+        }
+        return -1;
+    }
 }
--- BlueCourse/src/L11_BinarySearch/ThePlayboyChimp.java
+++ BlueCourse/src/L11_BinarySearch/ThePlayboyChimp.java
@@ -3,5 +3,68 @@
  * */
 package L11_BinarySearch;
 
+import java.util.Scanner;
+
 public class ThePlayboyChimp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int[] height = new int[N];
+        for(int i = 0; i < N; i++){
+            height[i] = scanner.nextInt();
+        }
+        int Q = scanner.nextInt();
+        for(int i = 0; i < Q; i++){
+            int chimpHeight = scanner.nextInt();
+            int shorter = printShorter(height, chimpHeight);
+            int higher = printHigher(height, chimpHeight);
+           if(shorter == -1){
+               System.out.print("X ");
+           }else {
+               System.out.print(shorter + " ");
+           }
+
+           if(higher == -1){
+               System.out.print("X");
+           }else {
+               System.out.print(higher);
+           }
+            System.out.println();
+        }
+    }
+
+    public static int printShorter(int[] height, int x){
+        int left = 0;
+        int right = height.length - 1;
+        int result = -1;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(x > height[mid]){
+                result = height[mid];
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    public static int printHigher(int[] height, int x){
+        int left = 0;
+        int right = height.length - 1;
+        int result = -1;
+
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(x < height[mid]){
+                result = height[mid];
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+
 }
